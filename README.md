68b7ca50bce5532628cde7b00a4c1adfbf823a3a
Fred's GitHub <stopchemtrailsfred@gmail.com>
2025-03-29 14:40:45 +0100

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Aeon-Flexx-CSS
-A terminal-like CSS file
+
+A terminal-like CSS file. Try it now at: [Aeon-Flexx-CSS](https://stopc.github.io/Aeon-Flexx-CSS/)
---
+++ main.js
@@ -0,0 +1,61 @@
+const SPECIAL_CHARS = "▄▓█▒□░▪■▫▀";
+let specialCharIdx = 0;
+
+function getNextSpecialChar() {
+    specialCharIdx++;
+    return SPECIAL_CHARS[specialCharIdx % SPECIAL_CHARS.length];
+}
+
+function selfType(element) {
+    const src = element.innerText;
+    let srcIdx = 0;
+    let dest = "";
+    element.innerText = "⠀";
+
+    const typer = () => {
+        if (src.length + 1 === dest.length) {
+            element.innerText = dest.slice(0, -1);
+            return;
+        }
+
+        dest = (srcIdx === 0) ? dest : dest.slice(0, -1);
+        dest += src[srcIdx] + getNextSpecialChar();
+        srcIdx++;
+        element.innerText = dest;
+        setTimeout(typer, 0 + (Math.random() * 50));
+    };
+
+    setTimeout(typer, 400);
+}
+
+function initSelfTypingElements() {
+    const els = document.querySelectorAll(".self-type");
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                selfType(entry.target);
+                observer.unobserve(entry.target);
+            }
+        });
+    });
+    els.forEach(el => observer.observe(el));
+}
+
+function makeElementsClickableWithSpace() {
+    [
+        ...document.querySelectorAll("label"),
+        ...document.querySelectorAll("a.btn")
+    ].forEach(label => {
+        label.addEventListener("keydown", function (event) {
+            if (event.keyCode === 32 || event.key === " ") {
+                event.preventDefault();
+                this.click();
+            }
+        });
+    })
+}
+
+(function () {
+    initSelfTypingElements();
+    makeElementsClickableWithSpace();
+})();
